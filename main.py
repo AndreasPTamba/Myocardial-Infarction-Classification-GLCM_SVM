9c75fd23d549a2c95dcfa6a4f298d668f12fcc34
Andreas Panangian Tamba <120867430+AndreasPTamba@users.noreply.github.com>
2024-05-14 00:07:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
-from function-back import preprocess_image, extract_glcm_feature, predict_image
+import function-back.preprocess_image
+import function-back.extract_glcm_feature
+import function-back.predict_image
 import cv2
 import numpy as np
 
